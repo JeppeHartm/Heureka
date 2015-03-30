46373a4931a41ef25da12c5a80105744cc3f5cc7
jeppe.hartmund@gmail.com <jeppe.hartmund@gmail.com>
2015-03-30 09:11:24 +0200

    Initial

---
+++ InferenceEngine/Program.fs
@@ -0,0 +1,26 @@
+﻿open System.IO
+open System.Text
+open Microsoft.FSharp.Text.Lexing
+
+open Proplog
+open ProplogLexer
+open ProplogParser
+
+let parseString (text:string) =
+   let lexbuf = LexBuffer<_>.FromBytes(Encoding.UTF8.GetBytes(text))
+   try
+       ProplogParser.Main ProplogLexer.tokenize lexbuf
+   with e ->
+        let pos = lexbuf.EndPos
+        printfn "Error near line %d, character %d\n" pos.Line pos.Column
+        failwith "parser termination"
+
+let _ = 
+    printfn "Write a sentence in Propositional logic using the operators '-', 'v', '^', '<-', '->', '<->' and parentheses: "
+    let e = parseString " a ^ b <-> -c -> d"
+    //System.Console.ReadLine()
+    printfn "%O" (PrettyPrint e)
+    printfn "is clause: %O" (isClause e)
+    printfn "is cnf: %O" (isCNF e)
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code

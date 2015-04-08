96ec4a6d0d0f9b13c9a3a5eff92061370091f30f
Jeppe <jeppe.hartmund@gmail.com>
2015-04-08 15:49:00 +0200

    Implemented datatypes for representation of problem and nodes

--- Heureka/Problem.fs
+++ Heureka/Problem.fs
@@ -1,18 +1,21 @@
 ﻿module Heureka.Problem
 
-type Node (state) =
-    let mutable parent = None
-    let mutable action = None
-    let mutable path_cost = 0
-    member n.State = state
-    member n.Parent = parent
-    member n.Action = action
-    member n.Path_Cost = cost
-let ChildNode problem node action =
+type Node<'a,'b>(state:'a,parent:option<Node<'a,'b>>,action:'b,cost:int) =
+    let mutable _cost:int = 0
+    do 
+        _cost<-cost
+    member x.State = state
+    member x.Parent = parent
+    member x.Action = action
+    member x.Path_Cost = _cost
+    member x.Set_Cost c = _cost<-c
 
-type Problem (init,actions,trans_model,goal_test,path_costs) =
-    member x.Initial_State() = init
-    member x.Actions (node) = actions
-    member x.Transition (node,action) = trans_model
-    member x.Goal_Test (state) = goal_test
-    member x.Path_Cost (
+type Problem<'a,'b> (init:'a,actions:'a -> List<'b>,trans_model:'a*'b -> 'a,goal_test:'a->bool,step_cost:'a*'b -> int) =
+    member x.Initial_State = init
+    member x.Actions = actions
+    member x.Result = trans_model
+    member x.Goal_Test = goal_test
+    member x.Step_Cost = step_cost
+
+let ChildNode (problem:Problem<'a,'b>) (node:Node<'a,'b>) (action:'b) = 
+    new Node<'a,'b>(problem.Result(node.State,action),Some(node),action,node.Path_Cost + problem.Step_Cost(node.State,action))
